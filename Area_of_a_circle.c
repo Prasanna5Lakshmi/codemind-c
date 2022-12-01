5e8fdabdcf86392387f4599e9ae6205c8fc4748e
Prasanna Lakshmi <119514812+Prasanna5Lakshmi@users.noreply.github.com>
2022-12-01 21:20:42 +0530

    #include<stdio.h> int main() {     float r,A,pi=3.14;     //printf("Enter the radiusof circle");     scanf("%f",&r);     A=pi*r*r;     printf("%.2f", A);     printf(" ");
    }

---
+++ Area_of_a_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float r,A,pi=3.14;
+    //printf("Enter the radiusof circle");
+    scanf("%f",&r);
+    A=pi*r*r;
+    printf("%.2f", A);
+    printf("
+");
+    
+}
