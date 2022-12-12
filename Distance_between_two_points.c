b9c35764efd367de627803fb5a86196f5f1a92b7
Prasanna Lakshmi <119514812+Prasanna5Lakshmi@users.noreply.github.com>
2022-12-12 20:17:30 +0530

    #include<stdio.h> #include<math.h> int main() {     int x1, y1, x2, y2;     float distance;     scanf("%d%d", &x1, &y1);     scanf("%d%d", &x2, &y2);     distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) );     printf("%0.4f",distance); }

---
+++ Distance_between_two_points.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x1, y1, x2, y2;
+    float distance;
+    scanf("%d%d", &x1, &y1);
+    scanf("%d%d", &x2, &y2);
+    distance = sqrt( (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) );
+    printf("%0.4f",distance);
+}
