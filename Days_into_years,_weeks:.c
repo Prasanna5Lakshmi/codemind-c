73c0233aec1b6893ce8eb24bb9d1393141ac08f5
Prasanna Lakshmi <119514812+Prasanna5Lakshmi@users.noreply.github.com>
2022-12-12 20:29:36 +0530

    #include <stdio.h> int main() {     int days,years, weeks;     scanf("%d",&days);     // Converts days to years, weeks and days     years = (days/365);     weeks = ((days % 365)/7);     //days = days- ((years*365) + (weeks*7));     printf("%d ", years);     printf("%d ", weeks);   }

---
+++ Days_into_years,_weeks:.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+int main()
+{
+    int days,years, weeks;
+    scanf("%d",&days);
+    // Converts days to years, weeks and days
+    years = (days/365); 
+    weeks = ((days % 365)/7);
+    //days = days- ((years*365) + (weeks*7));
+    printf("%d
+", years);
+    printf("%d
+", weeks);
+  }
