506dd124684faa741012f72874ccebbce6bd4eff
Prasanna Lakshmi <119514812+Prasanna5Lakshmi@users.noreply.github.com>
2022-12-25 13:35:38 +0530

    #include<stdio.h> int main() {     int n,m,k;     scanf("%d %d %d", &n, & m, &k);     if ( n- (m*k) >0)     {         printf("NO");
        }
        else
        {
            printf("YES");
            
        }
        return 0;
    }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,k;
+    scanf("%d %d %d", &n, & m, &k);
+    if ( n- (m*k) >0)
+    {
+        printf("NO");
+        
+    }
+    else
+    {
+        printf("YES");
+        
+    }
+    return 0;
+}
