6a7cd8ad8616ab4d7f0f8dfaec550aa84d3a23fe
Prasanna Lakshmi <119514812+Prasanna5Lakshmi@users.noreply.github.com>
2022-12-30 10:40:10 +0530

    #include <stdio.h> int main() {     int n;     while(1){         scanf("%d",&n);         if(n==-1){             break;         }         else         {             printf("%d ",n*n);         }     }
      return 0;
    }

---
+++ Infinite.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n;
+    while(1){
+        scanf("%d",&n);
+        if(n==-1){
+            break;
+        }
+        else
+        {
+            printf("%d
+",n*n);
+        }
+    }
+
+  return 0;
+}
